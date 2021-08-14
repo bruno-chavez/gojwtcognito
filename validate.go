ff6f3b8f4502063ded7347923f079524292f2a3a
bruno-chavez <bruno.chavezl@sansano.usm.cl>
2021-08-14 13:06:40 -0400

    Moved validation logic outside of ValidateTokenFromHeader to be used by GetClaims as well

--- validate.go
+++ validate.go
@@ -1,39 +1,17 @@
 package gojwtcognito
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // ValidateTokenFromHeader parses a request header and looks for a specific JWT from AWS Cognito.
-// Returns an error if its not valid or nil if it is.
+// Returns an error if it's not valid or nil if it is.
 // Use this function when you only need to check if a token is valid or not.
 func (c CognitoChecker) ValidateTokenFromHeader(request *http.Request, tokenType string) error {
 
-	claims, err := c.GetClaims(request, tokenType)
+	_, err := c.GetClaims(request, tokenType)
 	if err != nil {
 		return err
-	}
-
-	// verifies token_use, issuer and target audience inside a JWT
-	switch {
-	case tokenType == "idToken":
-		if claims["token_use"] != "id" {
-			return fmt.Errorf("invalid token use: %v", claims["token_use"])
-		}
-
-		if claims["aud"] != c.appClient {
-			return fmt.Errorf("invalid target audience: %v", claims["aud"])
-		}
-	case tokenType == "accessToken":
-		if claims["token_use"] != "access" {
-			return fmt.Errorf("invalid token use: %v", claims["token_use"])
-		}
-	}
-
-	iss := fmt.Sprintf("https://cognito-idp.%v.amazonaws.com/%v", c.region, c.userPool)
-	if claims["iss"] != iss {
-		return fmt.Errorf("invalid issuer: %v", claims["iss"])
 	}
 
 	return nil
