99be27be6bcc3974e5db46196ff356e6ec26c869
bruno-chavez <bruno.chavezl@sansano.usm.cl>
2021-08-14 13:09:46 -0400

    Removed JWT specific logic out of internal methods to rely on lestrrat-go/jwx

--- internal.go
+++ internal.go
@@ -1,67 +1,28 @@
-// Package gojwtcognito is an easy to use, small package designed to parse request headers and look for
-// JWTs provided by AWS Cognito to either check if they are valid or get some data from them.
+// Package gojwtcognito is an easy-to-use, small wrapper over 'github.com/lestrrat-go/jwx/jwk'
+// to provide specific functionality to work with AWS Cognito JWTs
 package gojwtcognito
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/lestrrat-go/jwx/jwk"
 	"net/http"
-	"strings"
 )
 
-// fetches from the request header all the necessary info to get the cookie info
-func getCookie(request *http.Request, appClient string, tokenType string) (*http.Cookie, error) {
+// Parses a http request to get the JWT
+func (c CognitoChecker) getJWT(request *http.Request, tokenType string) ([]byte, error) {
 
-	userIdCookie := fmt.Sprintf("CognitoIdentityServiceProvider.%v.LastAuthUser", appClient)
-	userId, err := request.Cookie(userIdCookie)
+	lastAuthUserKey := fmt.Sprintf("CognitoIdentityServiceProvider.%v.LastAuthUser", c.appClient)
+	userId, err := request.Cookie(lastAuthUserKey)
 	if err != nil {
-		return nil, fmt.Errorf("could not find cookie with name: %v", userIdCookie)
+		return nil, fmt.Errorf("could not find 'LastAuthUser' cookie with name: %v", lastAuthUserKey)
 	}
 
-	tokenName := fmt.Sprintf("CognitoIdentityServiceProvider.%v.%v.%v", appClient, userId.Value, tokenType)
+	tokenName := fmt.Sprintf("CognitoIdentityServiceProvider.%v.%v.%v", c.appClient, userId.Value, tokenType)
 	cookie, err := request.Cookie(tokenName)
 	if err != nil {
-		return nil, fmt.Errorf("could not find cookie with name: %v", tokenName)
+		return nil, fmt.Errorf("could not find '%v' cookie with name: %v", tokenType, tokenName)
 	}
 
-	return cookie, nil
-}
-
-// segment is used to unmarshall a part of cookie
-type segment struct {
-	Kid string `json:"kid"`
-}
-
-// getKey builds the key using the jwks and the first part of the specified cookie in order to match key ids
-func getKey(cookie *http.Cookie, jwks *jwk.Set) (interface{}, error) {
-
-	// the key id of a JWT is in the first part of the value
-	tokenIdSegment := strings.Split(cookie.Value, ".")[0]
-	decodedIdSegment, err := jwt.DecodeSegment(tokenIdSegment)
-	if err != nil {
-		return nil, err
-	}
-
-	var seg segment
-	err = json.Unmarshal(decodedIdSegment, &seg)
-	if err != nil {
-		return nil, err
-	}
-
-	// matches key id from the cookie and the jwks
-	keys := jwks.LookupKeyID(seg.Kid)
-	if len(keys) == 0 {
-		return nil, fmt.Errorf("failed to look up: %v", seg.Kid)
-	}
-
-	key, err := keys[0].Materialize()
-	if err != nil {
-		return nil, err
-	}
-
-	return key, nil
+	return []byte(cookie.Value), nil
 }
 
 func (c CognitoChecker) validateJWT(claims map[string]interface{}, tokenType string) error {
