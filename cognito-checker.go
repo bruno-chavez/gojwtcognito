602d67019c566d3f4ff7de183c0d67b9aef7ff24
bruno-chavez <bruno.chavezl@sansano.usm.cl>
2021-08-14 13:10:44 -0400

    Updated NewCognitoChecker to accomodate for breaking changes on lestrrat-go/jwx/jwk

--- cognito-checker.go
+++ cognito-checker.go
@@ -1,6 +1,7 @@
 package gojwtcognito
 
 import (
+	"context"
 	"fmt"
 	"github.com/lestrrat-go/jwx/jwk"
 )
@@ -11,25 +12,25 @@ type CognitoChecker struct {
 	region    string
 	userPool  string
 	appClient string
-	jwks      *jwk.Set
+	jwks      jwk.Set
 }
 
-// NewCognitoChecker is used for generating a CognitoChecker object and been able to use the library
-// Needs the region, user pool id and app client id of your Cognito user pool to work properly
+// NewCognitoChecker is used for generating a CognitoChecker object and been able to use the library.
+// Needs the region, user pool id and app client id of your Cognito user pool to work.
 func NewCognitoChecker(region, userPool, appClient string) (*CognitoChecker, error) {
-	cognitoChecker := &CognitoChecker{}
-
-	cognitoChecker.region = region
-	cognitoChecker.userPool = userPool
-	cognitoChecker.appClient = appClient
+	cognitoChecker := &CognitoChecker{
+		region:    region,
+		userPool:  userPool,
+		appClient: appClient,
+	}
 
 	jwkURL := fmt.Sprintf("https://cognito-idp.%v.amazonaws.com/%v/.well-known/jwks.json",
 		region,
 		userPool)
 
-	jwks, err := jwk.Fetch(jwkURL)
+	jwks, err := jwk.Fetch(context.TODO(), jwkURL)
 	if err != nil {
-		return cognitoChecker, err
+		return cognitoChecker, fmt.Errorf("error fetching jwks: %w", err)
 	}
 	cognitoChecker.jwks = jwks
 
